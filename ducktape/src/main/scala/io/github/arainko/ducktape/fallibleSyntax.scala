99e09448b6239077790a72409b0fb1e2ddd65170
Aleksander Rainko <aleksander.rainko99@gmail.com>
2023-05-02 15:26:11 +0200

    Fixes #54
    Move the contents of `fallibleSyntax.scala` to `syntax.scala`

--- ducktape/src/main/scala/io/github/arainko/ducktape/fallibleSyntax.scala
+++ ducktape/src/main/scala/io/github/arainko/ducktape/fallibleSyntax.scala
@@ -1,26 +1,4 @@
 package io.github.arainko.ducktape
 
-import io.github.arainko.ducktape.fallible.FallibleTransformer
-import io.github.arainko.ducktape.function.*
-import io.github.arainko.ducktape.internal.macros.{ Errors, Transformations }
-
-import scala.deriving.Mirror
-
-extension [F[+x], Source](value: Source)(using F: Transformer.Mode[F]) {
-
-  inline def fallibleTo[Dest](using transformer: FallibleTransformer[F, Source, Dest]): F[Dest] =
-    transformer.transform(value)
-
-  inline def fallibleVia[Func](inline function: Func)(using
-    Func: FunctionMirror[Func]
-  )(using Source: Mirror.ProductOf[Source]): F[Func.Return] =
-    inline F match {
-      case given fallible.Mode.FailFast[F] =>
-        Transformations.failFastVia[F, Source, Func](function)(value)
-      case given fallible.Mode.Accumulating[F] =>
-        Transformations.accumulatingVia[F, Source, Func](function)(value)
-      case other =>
-        Errors.cannotDetermineTransformationMode
-    }
-
-}
+private[ducktape] final class fallibleSyntax$package
+private[ducktape] object fallibleSyntax$package
--- ducktape/src/main/scala/io/github/arainko/ducktape/syntax.scala
+++ ducktape/src/main/scala/io/github/arainko/ducktape/syntax.scala
@@ -5,6 +5,7 @@ import io.github.arainko.ducktape.function.*
 import io.github.arainko.ducktape.internal.macros.*
 
 import scala.deriving.Mirror
+import io.github.arainko.ducktape.fallible.FallibleTransformer
 
 extension [Source](value: Source) {
   def into[Dest]: AppliedBuilder[Source, Dest] = AppliedBuilder(value)
@@ -19,3 +20,23 @@ extension [Source](value: Source) {
     Source: Mirror.ProductOf[Source]
   ): Func.Return = Transformations.via(value, function)
 }
+
+extension [F[+x], Source](value: Source)(using F: Transformer.Mode[F]) {
+
+  inline def fallibleTo[Dest](using transformer: FallibleTransformer[F, Source, Dest]): F[Dest] =
+    transformer.transform(value)
+
+  inline def fallibleVia[Func](inline function: Func)(using
+    Func: FunctionMirror[Func]
+  )(using Source: Mirror.ProductOf[Source]): F[Func.Return] =
+    inline F match {
+      case given fallible.Mode.FailFast[F] =>
+        Transformations.failFastVia[F, Source, Func](function)(value)
+      case given fallible.Mode.Accumulating[F] =>
+        Transformations.accumulatingVia[F, Source, Func](function)(value)
+      case other =>
+        Errors.cannotDetermineTransformationMode
+    }
+
+}
+
